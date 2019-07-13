946496621573d25f7137d3b49bfc77dfab6773ae
Igor Machado <igor.machado@gmail.com>
2019-07-12 21:35:09 -0300

    failing compare

--- tests/csBigInteger.Test.cpp
+++ tests/csBigInteger.Test.cpp
@@ -1,6 +1,7 @@
 
 // collection of 'csBigInteger' project tests
 
+#include "./arithmetics-tests/arithmetics.Test.cpp"
 #include "./helper-tests/helper.Test.cpp"
 #include "./serialize-tests/serialize.Test.cpp"
 
